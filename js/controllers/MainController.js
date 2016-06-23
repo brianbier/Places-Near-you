ef1f37a38ea813e73aba0c1b68f36dda311aba72
Brian Bier <briansbier@gmail.com>
2016-06-23 11:41:20 -0400

    Set up angular and the leaflet map

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,14 @@
-app.controller('MainController',['$scope',function($scope){
-
+app.controller('MainController',['$scope','places',function($scope, places){
+    $scope.mapCenter = {
+        lat: 40.856279, 
+        lng: -73.937328,
+        zoom: 17
+    }
+    
+    // Calls the places service success function, and once it receives the data, places it into the $scope.geodata variable, which can then be used in the view.
+    places.success(function(data) {
+        $scope.geodata = data;
+        // The geodataToMarkers function comes from the leaflet-directive module. We included that as a module in app.js
+        $scope.mapMarkers = geodataToMarkers($scope.geodata);
+    });
 }]);
